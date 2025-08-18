a5c9b3a89ccb16f4e1fa47cc46c9fb10e74f5d54
leekyuhyun <leekh010502@gmail.com>
2025-08-18 15:12:59 +0900

    chore:프로젝트 환경 설정 및 기본 구성

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/node_modules/**'],
+  },
+  {
+    name: 'app/global-config',
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
+  js.configs.recommended,
+  {
+    name: 'app/custom-rules',
+    rules: {
+      'no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'vue/multi-word-component-names': 'warn',
+      'vue/no-unused-vars': 'error',
+      'vue/require-v-for-key': 'error',
+      'vue/no-mutating-props': 'error',
+      'vue/no-reserved-component-names': 'off',
+    },
+  },
+  skipFormatting,
+]
