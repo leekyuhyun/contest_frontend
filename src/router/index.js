cbfe1819985332a6a1c18316de1fca1e35b90acd
leekyuhyun <leekh010502@gmail.com>
2025-08-18 17:36:46 +0900

    feat: 웹 페이지 이동 라우터

---
+++ src/router/index.js
@@ -0,0 +1,61 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import DeviceRegistration from '@/views/DeviceRegistration.vue'
+import DeviceListView from '@/views/DeviceListView.vue'
+import DeviceDetailView from '../views/DeviceDetailView.vue'
+import Monitoring from '@/views/Monitoring.vue'
+import EmergencyDashboard from '@/views/EmergencyDashboard.vue'
+import SituationAlertDashboard from '@/views/SituationAlertDashboard.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+
+  {
+    path: '/register-device',
+    name: 'device-registration',
+    component: DeviceRegistration,
+  },
+
+  {
+    path: '/device-list',
+    name: 'device-list',
+    component: DeviceListView,
+  },
+
+  {
+    path: '/device/:deviceId',
+    name: 'device-detail',
+    component: DeviceDetailView,
+    props: true,
+  },
+
+  {
+    path: '/monitoring',
+    name: 'monitoring',
+    component: Monitoring,
+  },
+
+  {
+    path: '/emergency/:macAddress',
+    name: 'emergency-dashboard',
+    component: EmergencyDashboard,
+    props: true,
+  },
+
+  {
+    path: '/alerts',
+    name: 'situation-alerts',
+    component: SituationAlertDashboard,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
