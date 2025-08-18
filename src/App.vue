9f0c79ce3af49fb8abc9f0445093d64aa3d0836f
leekyuhyun <leekh010502@gmail.com>
2025-08-18 15:18:57 +0900

    feat: 프로젝트 기본 세팅

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="d-flex flex-column min-vh-100">
+    <AppHeader />
+    <div class="flex-grow-1">
+      <router-view />
+    </div>
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/layout/Header.vue'
+import AppFooter from './components/layout/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+}
+</script>
